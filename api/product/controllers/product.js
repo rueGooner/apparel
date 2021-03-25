56e27861082faa803af031e413ab9d1f5ab34763
Rueben <rueben.cumberbatch@jdplc.com>
2021-03-25 19:55:24 +0000

    create brand

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,8 +1,25 @@
 'use strict';
+const faker = require('faker');
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+async function generateProduct() {
+  return {
+    name: faker.commerce.productName(),
+    product_description: faker.commerce.productDescription(),
+    price: faker.commerce.price()
+  }
+}
+
+module.exports = {
+  async create(ctx) {
+    let products = [];
+    for (let i = 0; i < faker.random.number({ min: 1000, max: 4000 }); i++) {
+
+    }
+    console.log('CREATE');
+  }
+};
